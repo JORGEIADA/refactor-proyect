9816f246f49fff25afdf23a26d864dd2fe1f5648
ada.jorgei <ada.jorgei@gmail.com>
2021-01-09 17:58:04 -0600

    first commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,17 @@
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height="300px".width="300px"
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor="blue"
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor="white"
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height="150px"
+    document.getElementById("box").style.backgroundColor="orange"
+})
---
+++ Module-6/newFile.js
@@ -0,0 +1,9 @@
+<button1 onclick="myfunction()">click me</button1>
+
+   ,
+
+   <p id="box"></p>
+   ,
+   <script>
+      document.getElementById("box").innerHTML ="hello world"
+   </script>;
